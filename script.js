29f0c089ab13466349ea765d876da2a22661c852
jaimezzapata <zapataval2304@gmail.com>
2025-09-23 12:54:14 -0500

    Se finaliza función de registrar estudiantes

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ let nombresEstudiantes = []
 let documentosEstudiantes = []
 let notasEstudiantes = []
 
-let registrarEstudiante = () => {}
-let buscarEstudiante = () => {}
-let filtrarEstudiantesGanaron = () => {}
-let filtrarEstudiantesPerdieron = () => {}
-let filtrarEstudiantesConPlanMejoramiento = () => {}
+let registrarEstudiante = () => {
+    let repetir = true
+    while (repetir) {
+        let nombreEstudiante = prompt("Ingrese el nombre del estudiante: ")
+        let documentoEstudiante = prompt("Ingrese el documento del estudiante: ")
+        let notaEstudiante = prompt("Ingrese la nota del estudiante: ")
+        nombresEstudiantes.push(nombreEstudiante)
+        documentosEstudiantes.push(documentoEstudiante)
+        notasEstudiantes.push(notaEstudiante)
+        let opcion = prompt("¿Desea registrar un nuevo estudiantes?\n1 = Si\n2 = No")
+        if (opcion == "2") {
+            repetir = false
+        }
+    }
+}
+let buscarEstudiante = () => { }
+let filtrarEstudiantesGanaron = () => { }
+let filtrarEstudiantesPerdieron = () => { }
+let filtrarEstudiantesConPlanMejoramiento = () => { }
+
+
+
